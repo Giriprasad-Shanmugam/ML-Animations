d8034c8426fcf3548d84f98e0a5bea0059a333d5
Giriprasad-Shanmugam <toogiriprasad@gmail.com>
2025-08-11 21:35:32 +0530

    Update app4.py

--- app4.py
+++ app4.py
@@ -81,7 +81,7 @@ for x, y in zip(x_data, y_data):
 ax.text(10.5, 14.5, f"SSE: {sse:.2f}", fontsize=12, color='black')
 
 # Show best fit line equation below SSE
-ax.text(10.5, 13.8, f"Best Fit: y = {m_best:.2f}x + {c_best:.2f}", fontsize=12, color='black')
+#ax.text(10.5, 13.8, f"Best Fit: y = {m_best:.2f}x + {c_best:.2f}", fontsize=12, color='black')
 
 # Streamlit outputs
 st.title("Interactive Simple Linear Regression")
