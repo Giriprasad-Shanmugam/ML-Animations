b63f6c8d6121a84b0fd1d2a83ca40391b26dcbfa
Giriprasad-Shanmugam <toogiriprasad@gmail.com>
2025-08-11 21:49:18 +0530

    Update app4.py

--- app4.py
+++ app4.py
@@ -37,9 +37,7 @@ find_best_fit = st.sidebar.button(
 # If button clicked → compute best fit line
 if find_best_fit:
     m, c = np.polyfit(x_data, y_data, 1)  # slope, intercept
-    # Show best fit line equation below SSE
-    ax.text(10.5, 13.8, f"Best Fit: y = {m_best:.2f}x + {c_best:.2f}", fontsize=12, color='black')
-
+   
 # Calculate regression line
 y_pred = m * x_data + c
 
@@ -81,7 +79,8 @@ for x, y in zip(x_data, y_data):
 ax.text(10.5, 14.5, f"SSE: {sse:.2f}", fontsize=12, color='black')
 
 # Show best fit line equation below SSE
-#ax.text(10.5, 13.8, f"Best Fit: y = {m_best:.2f}x + {c_best:.2f}", fontsize=12, color='black')
+if find_best_fit:
+    ax.text(10.5, 13.8, f"Best Fit: y = {m_best:.2f}x + {c_best:.2f}", fontsize=12, color='black')
 
 # Streamlit outputs
 st.title("Interactive Simple Linear Regression")
